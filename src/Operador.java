62290dbbc013a5745cc5212ec95ad831efcba81d
Ocante António <63821194+Ocante@users.noreply.github.com>
2023-06-02 16:33:34 -0300

    Debugar o código, ver valores  negativo e tornar esse numero positivo

--- src/Operador.java
+++ src/Operador.java
@@ -2,27 +2,34 @@ public class Operador {
     public static void main(String[] args) {
 
         // qual o resultado das expressoes abaixo?
-        String concatenacao = "?";
+        // String concatenacao = "?";
 
-        concatenacao = 1 + 1 + 1 + "1";
+        // concatenacao = 1 + 1 + 1 + "1";
 
-        System.out.println(concatenacao);
+        // System.out.println(concatenacao);
 
-        concatenacao = 1 + "1" + 1 + 1;
+        // concatenacao = 1 + "1" + 1 + 1;
 
-        System.out.println(concatenacao);
+        // System.out.println(concatenacao);
 
-        concatenacao = 1 + "1" + 1 + "1";
+        // concatenacao = 1 + "1" + 1 + "1";
 
-        System.out.println(concatenacao);
+        // System.out.println(concatenacao);
 
-        concatenacao = "1" + 1 + 1 + 1;
+        // concatenacao = "1" + 1 + 1 + 1;
 
-        System.out.println(concatenacao);
+        // System.out.println(concatenacao);
 
-        concatenacao = "1" + (1 + 1 + 1);
+        // concatenacao = "1" + (1 + 1 + 1);
 
-        System.out.println(concatenacao);
+        // System.out.println(concatenacao);
+
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
 
     }
 
