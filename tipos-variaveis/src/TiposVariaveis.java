72c4df7396b1cae1b967a2ee1d222cd8c7be920a
Ocante António <63821194+Ocante@users.noreply.github.com>
2023-06-02 20:28:58 -0300

    Tipos e Variáveis
    No Java, existem algumas palavras reservadas, para a representação dos tipos de dados básicos, que precisam ser manipulados, para a construção de programas. Estes tipos de dados são conhecidos como tipos primitivos (Primitive Types).
    Os oito tipos primitivos em Java são:
    int, byte, short, long, float, double, boolean e char – esses tipos não são considerados objetos e portanto representam valores brutos. Eles são armazenados diretamente na pilha de memória. (Memory stack).
    
    Aqui consta exemplos de uso de tipos de variáveis.

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,22 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        // byte idade = 123;
+        // short ano = 2021;
+        // int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        // long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro
+        // tipo
+        // float pi = 3.14F;
+        // double salario = 1275.33;
+        // TiposEVariaveis.java
+
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
+
+        // int numero = 10;
+
+        // System.out.print(numero);
+        // final double VALOR_DE_PI = 3.14;
+
+    }
+}
