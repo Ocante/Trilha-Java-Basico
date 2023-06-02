cd8dbd6a3858a76080e0d05b8f033e1c7ae24570
Ocante António <63821194+Ocante@users.noreply.github.com>
2023-06-02 16:22:59 -0300

    Concatenação de valores

---
+++ src/Operador.java
@@ -0,0 +1,29 @@
+public class Operador {
+    public static void main(String[] args) {
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+
+    }
+
+}
